4f51939e087dec4107874dcebacbe0ff753c74d4
Makarose <rosemarie.kaes@gmail.com>
2026-01-11 12:38:54 -0500

    Added keyboard input 1 & 2 for title page.

--- Scripts/title_page.gd
+++ Scripts/title_page.gd
@@ -7,3 +7,12 @@ func _on_male_pressed() -> void:
 func _on_female_pressed() -> void:
 	GameManager.chosen_character = "res://Scenes/female.tscn"
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and not event.echo:
+		match event.keycode:
+			KEY_1, KEY_KP_1:
+				_on_male_pressed()
+			KEY_2, KEY_KP_2:
+				_on_female_pressed()
